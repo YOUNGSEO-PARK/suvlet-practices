e4541e1e01e6bb4ed605fc4594773bae52243da8
YOUNGSEO-PARK <irene0seo97@gmail.com>
2020-07-29 17:11:18 +0900

    guestbook 'delete' func. create

--- guestbook02/src/main/java/com/bit2020/guestbook/dao/GuestbookDao.java
+++ guestbook02/src/main/java/com/bit2020/guestbook/dao/GuestbookDao.java
@@ -1,5 +1,4 @@
 package com.bit2020.guestbook.dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,37 +6,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.bit2020.guestbook.vo.GuestbookVo;
 
 public class GuestbookDao {
-
-	public boolean insert(GuestbookVo vo) {
+	public boolean delete(Long no, String password) {
 		boolean result = false;
 		Connection connection = null;
 		PreparedStatement pstmt = null;
 
 		try {
-	
 			// 1. 연결하기
 			connection = getConnection();
-			
+
 			// 2. SQL 준비
-			String sql = "insert into webdb.guestbook value(null, ?, ?, ?, now())"; 
+			String sql = "delete from guestbook where no = ? and password = ?" ; 
 			pstmt = connection.prepareStatement(sql);
 
 			// 3. 바인딩(binding)
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getMessage());
-			
-			// 4. sql 실행
-			int count = pstmt.executeUpdate(sql);
+			pstmt.setLong(1, no);
+			pstmt.setString(2, password);
+
+			// 4. sql 실행	
+			int count = pstmt.executeUpdate();
 			result = (count == 1);
 
 		} catch (SQLException e) {
 			System.out.println("에러:" + e);
-			
 		} finally {
 			try {
 				if(pstmt != null) {
@@ -50,22 +44,61 @@ public class GuestbookDao {
 				e.printStackTrace();
 			}
 		}
+
 		return result;
 	}
 
+	public boolean insert(GuestbookVo vo) {
+		boolean result = false;
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			// 1. 연결하기
+			connection = getConnection();
+			// 2. SQL 준비
+			String sql = "insert into guestbook value(null, ?, ?, ?, now())"; 
+			pstmt = connection.prepareStatement(sql);
+			
+			// 3. 바인딩(binding)
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getMessage());
+			
+			// 4. sql 실행	
+			int count = pstmt.executeUpdate();
+			result = (count == 1);
+			
+		} catch (SQLException e) {
+			System.out.println("에러:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
 	public List<GuestbookVo> findAll() {
 		List<GuestbookVo> result = new ArrayList<>();
 		
 		Connection connection = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-
+		
 		try {
-			// 1. 연결하기
+			// 1. 연결하기 
 			connection = getConnection();
 			
-			// 2.SQL 준비
-			String sql = "select no, name, message, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from webdb.guestbook order by reg_date";
+			// 2. SQL 준비
+			String sql = "select no, name, message, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc";
 			pstmt = connection.prepareStatement(sql);
 			
 			// 3. 바인딩
@@ -78,19 +111,17 @@ public class GuestbookDao {
 				String name = rs.getString(2);
 				String message = rs.getString(3);
 				String regDate = rs.getString(4);
-
+				
 				GuestbookVo vo = new GuestbookVo();
 				vo.setNo(no);
 				vo.setName(name);
 				vo.setMessage(message);
 				vo.setRegDate(regDate);
-
+				
 				result.add(vo);
 			}
-
 		} catch (SQLException e) {
 			System.out.println("에러:" + e);
-			
 		} finally {
 			try {
 				if(pstmt != null) {
@@ -102,26 +133,25 @@ public class GuestbookDao {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-		}
+		}		
 		
 		return result;
 	}
 	
-	private Connection getConnection() throws SQLException{
+	private Connection getConnection() throws SQLException {
 		Connection connection = null;
 		try {
-				// 1. JDBC Driver(MariaDB Driver)
-				Class.forName("org.mariadb.jdbc.Driver");
-		
-				// 2. 연결하기
-				String url = "jdbc:mysql://127.0.0.1:3306/webdb?characterEncoding=utf8";
-				connection = DriverManager.getConnection(url, "webdb", "webdb");
-		
+			// 1. JDBC Driver(MariaDB Driver)
+			Class.forName("org.mariadb.jdbc.Driver");
+	
+			// 2. 연결하기
+			String url = "jdbc:mysql://127.0.0.1:3306/webdb?characterEncoding=utf8";
+			connection = DriverManager.getConnection(url, "webdb", "webdb");
+			
 		} catch(ClassNotFoundException e) {
-
 			System.out.println("드라이버 로딩 실패");
 		}
-	
+		
 		return connection;
 	}
 }
